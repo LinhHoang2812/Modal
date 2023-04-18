e1ce54708142dc9f7c4c932a8c98733c6c06e372
LinhHoang2812 <zoeylinh2812@gmail.com>
2023-04-18 22:22:55 +0200

    first innit

---
+++ script.js
@@ -0,0 +1,32 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+window.addEventListener("resize", function () {
+  if (this.innerWidth > 800) {
+    sidebar.classList.remove("show-sidebar");
+  }
+});
+
+const products = document.querySelectorAll(".product");
+const modals = document.querySelectorAll(".modal-overlay");
+
+products.forEach(function (product) {
+  let image = product.querySelector(".btn-img");
+  image.addEventListener("click", function () {
+    let itemName = product.classList[1];
+    modals.forEach(function (modal) {
+      let modalName = modal.classList[1];
+      if (itemName === modalName) {
+        modal.classList.add("show-modal");
+      }
+      closeBtn = modal.querySelector(".close-btn");
+      closeBtn.addEventListener("click", function () {
+        modal.classList.remove("show-modal");
+      });
+    });
+  });
+});
